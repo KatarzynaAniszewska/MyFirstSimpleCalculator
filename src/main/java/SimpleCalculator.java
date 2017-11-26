50e0b33661d64959a1c3cce666b3441716acaa60
Katarzyna <katarzyna.aniszewska@gmail.com>
2017-11-26 14:00:21 +0100

    First Commit

---
+++ src/main/java/SimpleCalculator.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class SimpleCalculator {
+
+        public int addAtoB (int  a, int b ){
+            return a-b;
+
+        }
+        public int subtractAfromB (int  a, int b ){
+        return a - b;
+
+    }
+    public static void main (String[]args){
+            SimpleCalculator simpleCalculator = new SimpleCalculator();
+            int addResult = simpleCalculator.addAtoB(500,2633);
+            int substractResult = simpleCalculator.subtractAfromB(546,8569);
+
+
+
+
+        System.out.println(addResult);
+        System.out.println(substractResult);
+}}
+
